1b8850fe090992f450c6984d02c89c77cee1ed87
Kodatechh <kodatechproducts@gmail.com>
2026-08-19 21:32:24 -0300

    Harden fiscal queue trigger permissions

---
+++ supabase/migrations/20260820002100_harden_fiscal_queue_trigger_function.sql
@@ -0,0 +1,2 @@
+revoke execute on function public.queue_fiscal_document_for_paid_order() from public, anon, authenticated;
+grant execute on function public.queue_fiscal_document_for_paid_order() to service_role;
